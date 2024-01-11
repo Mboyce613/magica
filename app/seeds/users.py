4ba18dbc04c8d986e7245b7c2b697bd3365742e1
michael <michael.a.boyce999@gmail.com>
2024-01-10 21:38:53 -0800

    made exp not 0

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', first_name = 'Demo', last_name = 'Person', exp = 0)
+        username='Demo', email='demo@aa.io', password='password', first_name = 'Demo', last_name = 'Person', exp = 1)
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', first_name = 'Marnie', last_name = 'Person', exp = 0)
+        username='marnie', email='marnie@aa.io', password='password', first_name = 'Marnie', last_name = 'Person', exp = 1)
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', first_name = 'Bobbie', last_name = 'Person', exp = 0)
+        username='bobbie', email='bobbie@aa.io', password='password', first_name = 'Bobbie', last_name = 'Person', exp = 1)
 
     db.session.add(demo)
     db.session.add(marnie)
