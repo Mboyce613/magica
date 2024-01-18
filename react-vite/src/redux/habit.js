eb8ffc9ac8e48e35f7d4f636314824bf23f21fc1
mshuler <shulermj@gmail.com>
2024-01-17 21:48:13 -0800

    displaying habits

--- react-vite/src/redux/habit.js
+++ react-vite/src/redux/habit.js
@@ -7,8 +7,8 @@ export const loadHabits=(habits)=>({
     habits
 })
 
-export const getAllHabits = () => async (dispatch)=>{
-    const res = await fetch('/api/habits')
+export const getAllHabits = (userId) => async (dispatch)=>{
+    const res = await fetch(`/api/habits/${userId}`)
     // console.log(res.text(), '----------')
     if(res.ok){
         const data = await res.json()
@@ -24,11 +24,11 @@ const habitReducer = (state = {}, action)=>{
         case LOAD_Habits:
             newState = {}
             console.log(action.habits, '-----store')
-            if(action.habits && action.habits !== undefined){
-                // action.habits.forEach(ele => {
+            if(action.habits.habits && action.habits.habits !== undefined){
+                action.habits.habits.forEach(ele => {
 
-                //     newState[ele.id] = ele
-                // })
+                    newState[ele.id] = ele
+                })
             }else{
                 newState = null
             }
