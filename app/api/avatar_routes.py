d18c3789173f63156b4f971e320d92824c38ac8a
Gustavo <galaxy0816tavo@gmail.com>
2024-01-17 20:21:11 -0700

    dailies component started

--- app/api/avatar_routes.py
+++ app/api/avatar_routes.py
@@ -7,7 +7,7 @@ avatar_routes = Blueprint('avatars', __name__)
 
 @avatar_routes.route('/')
 
-# @login_required
+@login_required
 def avatars():
     """
     Query for all avatars and returns them in a list of avatar dictionaries
@@ -17,7 +17,7 @@ def avatars():
 
 
 @avatar_routes.route('/<int:id>')
-# @login_required
+@login_required
 def avatar(id):
     """
     Query for an avatar by id and returns that avatar in a dictionary
