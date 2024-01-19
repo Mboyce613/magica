e73ed301cfba9ceb18b0e4fe1c66b1c8c262862d
Gustavo <galaxy0816tavo@gmail.com>
2024-01-18 20:21:08 -0700

    daily component start

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -4,26 +4,27 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { getAllAvatars } from "../../redux/avatar";
 import { thunkSignup } from "../../redux/session";
 import { getAllBackgrounds } from "../../redux/backround";
+import DailiesComponent from "../DailiesComponent/DailiesCom";
 
 function HomePage(){
     const sessionUser = useSelector((state) => state.session.user);
     if(!sessionUser) return <Navigate to='signup' replace={true}/>
     const [isLoaded, setIsLoaded] = useState(false)
     const dispatch = useDispatch()
-    const avatars = useSelector((state)=>state.avatars)
-    const backgrounds = useSelector((state)=>state.backgrounds)
-    console.log(avatars)
-    useEffect(()=>{
-        dispatch(getAllAvatars())
-          .then(()=>{
-            setIsLoaded(true)
-          }
-            )
-    },[avatars, backgrounds, isLoaded])
+    // const avatars = useSelector((state)=>state.avatars)
+    // const backgrounds = useSelector((state)=>state.backgrounds)
+    // console.log(avatars)
+    // useEffect(()=>{
+    //     dispatch(getAllAvatars())
+    //       .then(()=>{
+    //         setIsLoaded(true)
+    //       }
+    //         )
+    // },[avatars, backgrounds, isLoaded])
     return (
-
-        <div>hello</div>
-
+        <div>
+          <DailiesComponent/>
+        </div>
     )
 }
 export default HomePage;
